3eadc861dad7270134f4631c73a5be4a4e85f921
Dominik <domrza99@gmail.com>
2022-12-31 16:49:08 +0100

    getting closer ...

--- D12/day12.js
+++ D12/day12.js
@@ -54,101 +54,64 @@ function part1(input) {
     .split("\n")
     .map((string) => {
       return string.split("").map((e) => {
-        if (e === "S") {
-          return 0;
-        } else if (e === "E") {
-          return "E";
-        } else {
-          let result = e.charCodeAt() - 96;
-          return result;
+        if (e === "S" || e === "E") return e;
+        else {
+          return e.charCodeAt() - 96;
         }
       });
     });
-  const queue = [[0, 0]];
-  const visited = new Array();
-  while (queue.length > 0) {
-    let current = queue.shift();
-    for (let i = 0; i <= 3; i++) {
-      let neighbour = neighbours(current).heights[i];
-      let index = neighbours(current).index[i];
-      let wall = neighbours(current).walls;
-      if (
-        visited.some((e) => {
-          index === null ? true : e[0] === index[0] && e[1] === index[1];
-        })
-      )
-        if (neighbour === "E" && graph[current[0]][current[1]] === 25) {
-          return (queue = []);
-        }
-
-      if (
-        wall &&
-        index !== null &&
-        !visited.some((e) => e[0] === current[0] && e[1] === current[1])
-      ) {
-        console.log(i);
-        switch (i) {
-          case 0: {
-            console.log("first");
-            queue.push(index);
-            visited.push(index);
-            break;
-          }
-          case 1: {
-            console.log("first");
-
-            queue.push(index);
-            visited.push(index);
-            break;
-          }
-          case 2: {
-            console.log("first");
-
-            queue.push(index);
-            visited.push(index);
-            break;
-          }
-          case 3: {
-            console.log("first");
-
-            queue.push(index);
-            visited.push(index);
-            break;
+  const adjList = () => {
+    const list = new Map();
+    let start, end;
+    for (let row = 0; row < graph.length; row++) {
+      for (let col = 0; col < graph[0].length; col++) {
+        const pos = graph[row][col];
+        const point = [row, col];
+        if (pos === "E") end = point;
+        if (pos === "S") start = point;
+        let neighbours = [
+          [row, col + 1],
+          [row, col - 1],
+          [row + 1, col],
+          [row - 1, col],
+        ];
+        for (const [nrow, ncol] of neighbours) {
+          if (
+            nrow < 0 ||
+            nrow >= graph.length ||
+            ncol < 0 ||
+            ncol >= graph[0].length
+          )
+            continue;
+          let nPos = graph[nrow][ncol];
+          if (nPos === "S") nPos = 1;
+          else if (nPos === "E") nPos = 26;
+          if (nPos - pos <= 1) {
+            if (!list.has(point)) list.set(point, []);
+            list.get(point).push([nrow, ncol]);
           }
         }
-        console.log(queue);
       }
     }
-  }
-
-  function neighbours(current) {
-    const left = current[1] > 0 ? graph[current[0]][current[1] - 1] : null;
-    const right =
-      current[1] < graph[0].length - 1
-        ? graph[current[0]][current[1] + 1]
-        : null;
-    const bottom =
-      current[0] < graph.length - 1 ? graph[current[0] + 1][current[1]] : null;
-    const top = current[0] > 0 ? graph[current[0] - 1][current[1]] : null;
-
-    const idxLeft = current[1] > 0 ? [current[0], current[1] - 1] : null;
-    const idxRight =
-      current[1] < graph[0].length - 1 ? [current[0], current[1] + 1] : null;
-    const idxTop = current[0] > 0 ? [current[0] - 1, current[1]] : null;
-    const idxBottom =
-      current[0] < graph.length - 1 ? [current[0] + 1, current[1]] : null;
-    const canLeft = left - graph[current[0]][current[1]] < 2 && left !== null;
-    const canRight =
-      right - graph[current[0]][current[1]] < 2 && right !== null;
-    const canBottom =
-      bottom - graph[current[0]][current[1]] < 2 && bottom !== null;
-    const canTop = top - graph[current[0]][current[1]] < 2 && top !== null;
-    return {
-      walls: [canLeft, canRight, canBottom, canTop],
-      heights: [left, right, bottom, top],
-      index: [idxLeft, idxRight, idxBottom, idxTop],
-    };
-  }
+    const shortestPathSeen = new Map();
+    shortestPathSeen.set(start, 0);
+    const queue = [[start, 0]];
+    console.log(queue);
+    while (queue.length > 0) {
+      const [currentPos, steps] = queue.shift();
+      const nextSteps = steps + 1;
+      for (const neighbor of list.get(currentPos)) {
+        if (
+          !shortestPathSeen.has(neighbor) ||
+          nextSteps < shortestPathSeen.get(neighbor)
+        ) {
+          shortestPathSeen.set(neighbor, nextSteps);
+          if (neighbor !== end) queue.push([neighbor, nextSteps]);
+        }
+      }
+    }
+    return shortestPathSeen.has(end) ? shortestPathSeen.get(end) : Infinity;
+  };
+  console.log(adjList());
 }
-console.log(part1(sample));
-let example = new Array([0, 1], [0, 2]);
+part1(sample);
